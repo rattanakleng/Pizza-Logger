f334703835c819a75072f7ef9da63c864351f191
Rattanak Leng <rleng@live.com>
2021-01-12 21:27:04 -0800

    update code insert start up data into pizzas table

---
+++ db/seeds.sql
@@ -0,0 +1,24 @@
+INSERT INTO
+  pizzas (name)
+VALUES
+  ("Veggie");
+INSERT INTO
+  pizzas (name)
+VALUES
+  ("Mediterranean");
+INSERT INTO
+  pizzas (name)
+VALUES
+  ("Chesse Avalanche");
+INSERT INTO
+  pizzas (name, devoured)
+VALUES
+  ("Philly Steak", true);
+INSERT INTO
+  pizzas (name, devoured)
+VALUES
+  ("Chicken Parm", true);
+INSERT INTO
+  pizzas (name, devoured)
+VALUES
+  ("Hawaiian", true);
