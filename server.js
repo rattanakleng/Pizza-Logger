d86c7755f55b28ae86d5bd8c74e791613f378f7e
Rattanak Leng <rleng@live.com>
2021-01-18 18:28:48 -0800

    add images, update server.js, add index.handlebars, main.handlebars, pizza-block.handlebars

--- server.js
+++ server.js
@@ -4,10 +4,10 @@ var PORT = process.env.PORT || 8080;
 
 var app = express();
 
-// Serve static content for the app from the "public" directory in the application directory.
+// Serve static content for the app from the "public" directory in the applipizzaion directory.
 app.use(express.static("public"));
 
-// Parse application body as JSON
+// Parse applipizzaion body as JSON
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
@@ -18,12 +18,13 @@ app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
 // Import routes and give the server access to them.
-var routes = require("./controllers/pizza_controller.js");
+var routes = require("./controllers/pizzasController.js");
 
 app.use(routes);
 
-// Start server so that it can begin listening to client requests.
-app.listen(PORT, function() {
-  // Log (server-side) when server has started
+// Start our server so that it can begin listening to client requests.
+app.listen(PORT, function () {
+
+  // Log (server-side) when our server has started
   console.log("Server listening on: http://localhost:" + PORT);
 });
