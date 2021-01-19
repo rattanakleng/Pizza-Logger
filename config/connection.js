8517aae06988d46049ee47108893ad1b5416cd04
Rattanak Leng <rleng@live.com>
2021-01-19 14:34:12 -0800

    change connection variable from const to let

--- config/connection.js
+++ config/connection.js
@@ -1,16 +1,9 @@
 // Set up MySQL connection
 const mysql = require ("mysql");
 
-// const connection = mysql.createConnection({
-//     host: "localhost",
-//     port: 3306,
-//     user: "root",
-//     password: "MySql@&2014",
-//     database: "pizza_db"
-// });
-
 //Hooking up project with JawsDB for heroku.
-const connection;
+let connection;
+
 if (process.env.JAWSDB_URL) {
     connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
@@ -19,7 +12,7 @@ if (process.env.JAWSDB_URL) {
         user: "root",
         password: "MySql@&2014",
         database:  "pizza_db"
-    })
+    });
 };
 
 // Make connection.
