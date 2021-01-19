55d1b29da0540cd7c0842d7016f1a554b10e9b84
Rattanak Leng <rleng@live.com>
2021-01-19 11:30:33 -0800

    add code to hookup project with JawsDB for Heroku

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,26 @@
 // Set up MySQL connection
 const mysql = require ("mysql");
 
-const connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "MySql@&2014",
-    database: "pizza_db"
-});
+// const connection = mysql.createConnection({
+//     host: "localhost",
+//     port: 3306,
+//     user: "root",
+//     password: "MySql@&2014",
+//     database: "pizza_db"
+// });
+
+//Hooking up project with JawsDB for heroku.
+const connection;
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host: "localhost",
+        user: "root",
+        password: "MySql@&2014",
+        database:  "pizza_db"
+    })
+};
 
 // Make connection.
 connection.connect(function(err) {
